e22a06306dba6bdb5595c5f081b5398c16006188
Linus Moreau <linus.moreau@gmail.com>
2020-08-15 16:23:07 -0700

    Add function to make into title Function receives a string and formats it as it should be if it were a title, capitalizing the appropriate words as per English grammar.

--- toolkit.py
+++ toolkit.py
@@ -21,10 +21,26 @@ def largest_in_dictionary(d):
 
 
 def capitalize(string):
-    string = str(string)
+    string = str(string).lower()
     if 97 <= ord(string[0]) <= 122:
         string = chr(ord(string[0]) - 32) + string[1:]
     return string
+
+
+def entitle(title):
+    syncategorematics = {'a', 'an', 'the', 'for', 'and', 'nor', 'but', 'or', 'yet', 'so', 'as', 'at', 'by', 'in', 'of',
+                         'on', 'per', 'to', 'with', 'into'}
+    title = str(title)
+    title.replace('_', ' ')
+    words = title.split()
+    title = capitalize(words.pop(0))
+    for word in words:
+        title += ' '
+        if word.lower() not in syncategorematics:
+            title += capitalize(word)
+        else:
+            title += word.lower()
+    return title
 
 
 def translate_bool_string(string, default=None):
