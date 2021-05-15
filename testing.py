5e08ef71d1f52f9dfa3bdbeb0e5bf96fe973c87a
Linus Moreau <linus.moreau@gmail.com>
2021-05-14 23:20:58 -0700

    Add additional test data and adapt decoder to read new data

--- testing.py
+++ testing.py
@@ -3,8 +3,8 @@ from toolkit import *
 import date_kit
 
 
-choice = 'Norway'
-view = 'parties'
+choice = 'Czechia'
+# view = 'parties'
 
 key = None
 
@@ -13,19 +13,33 @@ if choice == 'Germany':
     col = {'CDU/CSU': (0, 0, 0), 'Gr\u00fcne': (100, 161, 45), 'SPD': (235, 0, 31), 'FDP': (255, 237, 0),
            'AfD': (0, 158, 224), 'Linke': (190, 48, 117)}
     # coalitions = [['Gr\u00fcne', 'CDU/CSU'], ]
-    file_name = 'german_polling.txt'
+    file_name = 'test_data/german_polling.txt'
     spread = 7
+    start = 4
 elif choice == 'Norway':
     key = ['R', 'SV', 'MDG', 'Ap', 'Sp', 'V', 'KrF', 'H', 'FrP']
     col = {'R': (231, 52, 69), 'SV': (188, 33, 73), 'MDG': (106, 147, 37), 'Ap': (227, 24, 54), 'Sp': (0, 133, 66),
            'V': (17, 100, 104), 'KrF': (254, 193, 30), 'H': (135, 173, 215), 'FrP': (2, 76, 147)}
-    file_name = 'norway_polling.txt'
+    file_name = 'test_data/norway_polling.txt'
     spread = 30
+    start = 4
+elif choice == 'Peru':
+    key = ['Castillo', 'Fujimori', 'None', 'Undecided']
+    col = {'Castillo': (192, 10, 10), 'Fujimori': (255, 128, 0)}
+    file_name = 'test_data/peru_polling.txt'
+    spread = 7
+    start = 3
+elif choice == 'Czechia':
+    key = ['ANO', 'SPOLU', 'Pirati+STAN', 'SPD', 'KSCM', 'CSSD']
+    col = {'ANO': (38, 16, 96), 'SPOLU': (35, 44, 119), 'Pirati+STAN': (0, 0, 0), 'SPD': (33, 117, 187),
+           'KSCM': (204, 0, 0), 'CSSD': (236, 88, 0)}
+    file_name = 'test_data/czechia_polling.txt'
+    spread = 7
+    start = 4
 else:
     raise ValueError("No such choice.")
 
 cycle = len(key) + 7
-start = 4
 date = 1
 
 dat: Dict[str, Dict[int, List[float]]] = {}
@@ -35,42 +49,66 @@ f = open(file_name, 'r')
 
 rot = None
 end = 0
+year = '2021'
+restart = '[http'
 
-for line in f:
-    if line[:2] == '|}':
+content = f.readlines()
+i = 0
+while i < len(content):
+    line = content[i]
+    print(rot, line, end='')
+    if '===' in line:
+        year = line.split()[1]
+    elif line[:2] == '|}':
         rot = None
     elif rot is None:
-        if 'http' in line:
+        if restart in line:
             rot = 1
     else:
         if rot == 1:
-            lin = line[:line.find('{')]
-            dates = lin.split('|')[-1]
-            temp = dates.split('â€“')[-1].strip()
+            if choice == "Norway":
+                line = line[:line.find('{')]
+            elif choice == "Peru":
+                line = line.strip().strip('}')
+            elif choice == 'Czechia' and 'rowspan="2"' in line:
+                i += 20
+                rot += 2
+            dates = line.split('|')[-1]
+            if '-' in dates:
+                temp = dates.split('-')
+            else:
+                temp = dates.split('â€“')
+            temp = temp[-1].strip()
+            # print(temp)
+            if len(temp.split()) == 2:
+                temp = temp + ' ' + year
+            elif len(temp.split()) == 1:
+                temp = '1' + ' ' + temp + ' ' + year
             end_date = date_kit.Date(text=temp, form='dmy')
             end = date_kit.date_dif(date_kit.Date(2021, 1, 1), end_date)
             # print('\n' + str(end), end_date)
         elif start <= rot < start + len(key):
             p = rot - start
-            if "style=" in line:
+            if "'''" in line:
                 share = float(line.split('|')[-1].strip().strip("'"))
             else:
-                share = float(line.strip('|').strip())
+                share = float(line.split('|')[-1].strip())
             # print(key[rot], share)
             if end in dat[key[p]]:
                 dat[key[p]][end].append(share)
             else:
                 dat[key[p]][end] = [share]
-        elif 'http' in line or '[[' in line:
+        elif restart in line:
             rot = 0
         rot += 1
+    i += 1
 f.close()
 
 # print(dat)
 
 ndat = rolling_averages(dat, spread)
 date = Date(2021, 1, 1)
-title = "Opinion Polling for the 2021 Federal Election in " + choice
+title = "Opinion Polling for " + choice
 graph = GraphDisplay(screen_center, (screen_width, screen_height), ndat, x_title=None, y_title="Support (%)",
                      title=title, step=1, align=CENTER, colours=col, initial_date=date, leader=True, y_min=0,
                      dat_points=dat)
