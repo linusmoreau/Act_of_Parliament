288202970d7fd2d1c219bcb75e5b2cd1b6a0a31d
Linus Moreau <linus.moreau@gmail.com>
2021-05-19 21:40:02 -0700

    Add updated and new test data and adapt the algorithm for reading the new data

--- testing.py
+++ testing.py
@@ -3,10 +3,11 @@ from toolkit import *
 import date_kit
 
 
-choice = 'Germany'
+choice = 'Norway'
 # view = 'parties'
 restart = '[http'
 key = None
+date = 1
 
 if choice == 'Germany':
     key = ['CDU/CSU', 'SPD', 'AfD', 'FDP', 'Linke', 'Gr\u00fcne']
@@ -14,7 +15,7 @@ if choice == 'Germany':
            'AfD': (0, 158, 224), 'Linke': (190, 48, 117)}
     # coalitions = [['Gr\u00fcne', 'CDU/CSU'], ]
     file_name = 'test_data/german_polling.txt'
-    spread = 14
+    spread = 30
     start = 4
 elif choice == 'Norway':
     key = ['R', 'SV', 'MDG', 'Ap', 'Sp', 'V', 'KrF', 'H', 'FrP']
@@ -27,7 +28,7 @@ elif choice == 'Peru':
     key = ['Castillo', 'Fujimori']
     col = {'Castillo': (192, 10, 10), 'Fujimori': (255, 128, 0)}
     file_name = 'test_data/peru_polling.txt'
-    spread = 14
+    spread = 30
     start = 3
     restart = 'http'
 elif choice == 'Czechia':
@@ -35,13 +36,18 @@ elif choice == 'Czechia':
     col = {'ANO': (38, 16, 96), 'SPOLU': (35, 44, 119), 'Pirati+STAN': (0, 0, 0), 'SPD': (33, 117, 187),
            'KSCM': (204, 0, 0), 'CSSD': (236, 88, 0)}
     file_name = 'test_data/czechia_polling.txt'
-    spread = 30
+    spread = 60
     start = 4
+elif choice == 'Canada':
+    key = ['CON', 'LIB', 'NDP', 'BQ', 'GPC', 'PPC']
+    col = {'CON': (100, 149, 237), 'LIB': (234, 109, 106), 'NDP': (244, 164, 96), 'BQ': (135, 206, 250),
+           'GPC': (153, 201, 85), 'PPC': (131, 120, 158)}
+    file_name = 'test_data/canada_polling.txt'
+    spread = 60
+    start = 3
 else:
     raise ValueError("No such choice.")
 
-cycle = len(key) + 7
-date = 1
 
 dat: Dict[str, Dict[int, List[float]]] = {}
 for n in key:
@@ -63,8 +69,8 @@ while i < len(content):
         rot = None
     elif rot is None:
         if restart in line:
-            rot = 1
-    else:
+            rot = 0
+    if rot is not None:
         if rot == 1:
             if choice == "Norway":
                 line = line[:line.find('{')]
@@ -88,6 +94,31 @@ while i < len(content):
             end_date = date_kit.Date(text=temp, form='dmy')
             end = date_kit.date_dif(date_kit.Date(2021, 1, 1), end_date)
             # print('\n' + str(end), end_date)
+        elif rot == 0 and choice == 'Canada':
+            # print(line)
+            parts = line.split('||')
+            temp = parts[date].split('|')[-1].strip().strip('}')
+            end_date = date_kit.Date(text=temp, form='mdy')
+            end = date_kit.date_dif(date_kit.Date(2021, 1, 1), end_date)
+            for n, p in enumerate(key):
+                if "''" in parts[start + n]:
+                    share = float(parts[start + n].split('|')[-1].strip().strip("'"))
+                else:
+                    num = parts[start + n].split('|')[-1].strip()
+                    if num == '{{n/a}}' or num == '' or \
+                            parts[start + n].split('|')[0].strip() == 'style="color:#F8F9FA;"':
+                        continue
+                    else:
+                        share = float(num)
+                if end in dat[p]:
+                    dat[p][end].append(share)
+                else:
+                    dat[p][end] = [share]
+            if "2019 Canadian federal election" in line:
+                key = ['LIB', 'CON', 'NDP', 'BQ', 'GPC', 'PPC']
+            i += 1
+            rot = None
+            continue
         elif start <= rot < start + len(key):
             p = rot - start
             if "'''" in line:
@@ -107,7 +138,7 @@ f.close()
 
 # print(dat)
 
-ndat = rolling_averages(dat, spread, True)
+ndat = weighted_averages(dat, spread, power=1)
 date = Date(2021, 1, 1)
 title = "Opinion Polling for " + choice
 graph = GraphDisplay(screen_center, (screen_width, screen_height), ndat, x_title=None, y_title="Support (%)",
