5e1d7e4e11465a8336a3fcd2a5dda3725d17f5c1
Linus Moreau <linus.moreau@gmail.com>
2021-06-26 17:32:27 -0700

    Fix bounding predicate for GraphDisplay curves

--- base_ui.py
+++ base_ui.py
@@ -1,6 +1,6 @@
-from typing import Dict, List, Set, Tuple, Any, Optional
-
 import os
+from typing import Dict, List, Any
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 import pygame.gfxdraw
@@ -1658,9 +1658,7 @@ class GraphDisplay(Widget):
     def moment(self, place):
         small = self.get_x_min() - self.x_min
         big = self.get_x_max() - self.x_min
-        if place < 0:
-            place = 0
-        elif place < small:
+        if place < small:
             place = small
         elif place > big:
             place = big
@@ -1922,7 +1920,8 @@ class GraphDisplay(Widget):
                 for y in ys:
                     p = (int(self.graph_rect.w + self.left_margin - ((self.x_max - x) * self.x_scale)),
                          int(self.rect.h - ((y - self.y_min) * self.y_scale + self.bottom_margin)))
-                    if self.in_container((p[0] + self.contain_rect.left, p[1] + self.contain_rect.top)):
+                    if self.graph_rect.x <= p[0] <= self.graph_rect.x + self.graph_rect.w and \
+                            self.graph_rect.y <= p[1] <= self.graph_rect.y + self.graph_rect.h:
                         pygame.gfxdraw.filled_circle(self.surface, p[0], p[1], 2, colour)
                         pygame.gfxdraw.aacircle(self.surface, p[0], p[1], 2, colour)
 
@@ -1937,7 +1936,8 @@ class GraphDisplay(Widget):
             for x in self.dat[line].keys():
                 p = (int(self.graph_rect.w + self.left_margin - ((self.x_max - x) * self.x_scale)),
                      int(self.rect.h - ((self.dat[line][x] - self.y_min) * self.y_scale + self.bottom_margin)))
-                if self.in_container((p[0] + self.contain_rect.left, p[1] + self.contain_rect.top)):
+                if self.graph_rect.x <= p[0] <= self.graph_rect.x + self.graph_rect.w and \
+                        self.graph_rect.y <= p[1] <= self.graph_rect.y + self.graph_rect.h:
                     points.append(p)
 
             for j in range(len(points) - 1):
